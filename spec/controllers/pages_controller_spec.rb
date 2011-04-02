10a0c518b717a9f2489c9186a9c3b506ee205670
Tony Perez <tony@yedgy.com>
2011-04-01 22:05:57 -0700

    Improved title tests

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -16,7 +16,7 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-                        :content => @base_title + " | Home")
+                                    :content => "#{@base_title} | Home")
     end
 
     it "should have a non-blank body" do
@@ -34,7 +34,7 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-                        :content => @base_title + " | Contact")
+                                    :content => "#{@base_title} | Contact")
     end
 
     it "should have a non-blank body" do
@@ -52,7 +52,7 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-                        :content => @base_title + " | About")
+                                    :content => "#{@base_title} | About")
     end
 
     it "should have a non-blank body" do
@@ -70,7 +70,7 @@ describe PagesController do
     it "should have the right title" do
       get 'help'
       response.should have_selector("title",
-                        :content => @base_title + " | Help")
+                                    :content => "#{@base_title} | Help")
     end
 
     it "should have a non-blank body" do
