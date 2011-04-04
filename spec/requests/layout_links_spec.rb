664d2075e738ea6a4a1850b75c6753ff077a632a
Tony Perez <tony@yedgy.com>
2011-04-03 22:37:06 -0700

    Added users controller

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -29,14 +29,19 @@ describe "LayoutLinks" do
     response.should have_selector('title', :content => "Contact")
   end
   
-  it "should have a Contact page at '/about'" do
+  it "should have an About page at '/about'" do
     get '/about'
     response.should have_selector('title', :content => "About")
   end
   
-  it "should have a Contact page at '/help'" do
+  it "should have a Help page at '/help'" do
     get '/help'
     response.should have_selector('title', :content => "Help")
+  end
+
+  it "should have a Sign Up page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign Up")
   end
 
 end
